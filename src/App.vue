2d8d27e12a08d8fc150fcf915e4190cc0cdb9479
Nabil-Alhanif <nabil.alhanif@protonmail.com>
2023-09-07 20:12:47 -0700

    Base authentication system

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+	import { ref, watchEffect } from 'vue' // used for conditional rendering
+	import { getAuth,onAuthStateChanged, signOut } from 'firebase/auth'
+	import { useRouter } from 'vue-router'
+
+	const router = useRouter()
+
+	const isLoggedIn = ref(true)
+
+	// runs after firebase is initialized
+	onAuthStateChanged(getAuth(),function(user) {
+			if (user) {
+				isLoggedIn.value = true // if we have a user
+			} else {
+				isLoggedIn.value = false // if we do not
+			}
+	})
+
+	const handleSignOut = () => {
+		signOut(getAuth())
+		router.push('/')
+	}
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+	<div>
+    <nav>
+      <router-link to="/"> Home </router-link> |
+      <span v-if="isLoggedIn"> 
+        <button @click="handleSignOut"> Logout </button> 
+      </span>
+      <span v-else>
+        <router-link to="/register"> Register </router-link> |
+        <router-link to="/sign-in"> Login </router-link>
+      </span>
+    </nav>
+    <router-view />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
+	{
+		path: '/',
+		component: () => import('../views/Home.vue')
+	},
+	{
+		path: '/register',
+		component: () => import('../views/Register.vue')
+	},
+	{
+		path: '/sign-in',
+		component: () => import('../views/SignIn.vue')
+	},
 ];
 
 const router = createRouter({
